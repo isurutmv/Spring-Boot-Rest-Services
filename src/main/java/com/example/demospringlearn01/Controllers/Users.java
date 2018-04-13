c0d142374de71c60c211fc90795a57baa137eeaa
isurutmv <isurutmv@gmail.com>
2018-04-13 09:17:24 +0530

    Enabled Cross Origin

--- src/main/java/com/example/demospringlearn01/Controllers/Users.java
+++ src/main/java/com/example/demospringlearn01/Controllers/Users.java
@@ -14,29 +14,31 @@ public class Users {
 
     @Autowired
     private UserServices userServices;
-
+    @CrossOrigin(origins = "http://localhost:8081")
     @GetMapping("/all")
     public List<UserEntity> getallusers(){
 
       return userServices.findAllUsers();
     }
-
+    @CrossOrigin(origins = "http://localhost:8081")
     @PostMapping("/add")
     public String adduser(@RequestBody UserEntity userEntity){
 
         return userServices.adduser(userEntity);
     }
-
+    @CrossOrigin(origins = "http://localhost:8081")
     @PutMapping("/update")
     public String updateuser(@RequestBody UserEntity userEntity){
 
         return userServices.updateuser(userEntity);
     }
+    @CrossOrigin(origins = "http://localhost:8081")
     @DeleteMapping("/delete")
     public String deleteuser(@RequestBody UserEntity userEntity){
 
         return userServices.deleteuser(userEntity);
     }
+    @CrossOrigin(origins = "http://localhost:8081")
     @GetMapping("/find/{id}")
     public Optional<UserEntity> finduser(@PathVariable Integer id)
     {
